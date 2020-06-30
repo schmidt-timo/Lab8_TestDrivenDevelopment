0d08ede8e3fc857be92067d29997d9c267385b5f
Timo Schmidt <schmidt@timo.email>
2020-06-30 15:22:47 +0200

    "Add method" added

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -3,14 +3,22 @@ public class LinkedList implements List{
 	
 	Node first;
 	Node next;
-	Node current;
+//	Node current;
 	Node last;
 	
 
 	@Override
 	public void add(Node node) {
 		// TODO Auto-generated method stub
-		
+		if (first == null) {
+			first = node;
+		}
+
+		else {
+			last.next = node;
+		}
+
+		last = node;
 	}
 
 	@Override
@@ -32,7 +40,24 @@ public class LinkedList implements List{
 	}
 	
 	public String toString() {
-		return "";
+		StringBuilder printedList = new StringBuilder();
+
+		if (first == null) {
+			return "";
+		}
+
+		Node current = first;
+
+		if (current.next == null) {
+			return current.data + "";
+		}
+
+		while (current.next != null) {
+			printedList.append(current.data);
+			current = current.next;
+		}
+
+		return printedList.toString();
 	}
 
 	
