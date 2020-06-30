abb77153a286efa15dd6e6947d557247e5736b57
Timo Schmidt <schmidt@timo.email>
2020-06-30 14:20:07 +0200

    initial commit

--- src/Node.java
+++ src/Node.java
@@ -1,5 +1,13 @@
 public class Node {
+    Node next;
+    Object data;
 
-    
+    public Node(Object data, Node next) {
+        this.data = data;
+        this.next = next;
+    }
 
+    public Node(Object data) {
+        this.data = data;
+    }
 }
